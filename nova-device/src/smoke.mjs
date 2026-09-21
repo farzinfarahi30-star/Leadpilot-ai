113dd95f066e77304e746c3ba735310f2b388405
farzinfarahi30-star <farzinfarahi30@gmail.com>
2026-09-21 20:03:00 +0330

    Add Nova device XX smoke tests

---
+++ nova-device/src/smoke.mjs
@@ -0,0 +1,10 @@
+import {deviceInfo,TOOL_REGISTRY,getConfig,getUsageStatistics} from './core.mjs';
+const info=deviceInfo();
+const required=['listDirectory','readFile','readMultipleFiles','getFileInfo','writeFile','editFileBlock','moveFile','createDirectory',
+  'searchFiles','getSearchResults','stopSearch','listActiveSearches','startProcess','sendInput','readProcessOutput','listActiveSessions',
+  'terminateSession','listSystemProcesses','killSystemProcess','writePdf','readExcel','writeExcel','termuxStatus','termuxProvision',
+  'termuxApi','termuxBackground','getConfig','setConfig','getUsageStatistics','getRecentToolActivity'];
+const missing=required.filter(x=>typeof TOOL_REGISTRY[x]!=='function');
+if(missing.length)throw new Error('Missing tools: '+missing.join(','));
+console.log('NOVA_DEVICE_XX_TESTS_PASS');
+console.log(JSON.stringify({platform:info.platform,termux:info.termux,toolCount:Object.keys(TOOL_REGISTRY).length,usage:await getUsageStatistics()},null,2));
